bfc4fbe997a105a0671ba44bb84f4f7a2550f6e8
Maia <maianicolecampeanu@gmail.com>
2026-09-16 21:16:57 +0300

    Add ADMO navigation shell

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from 'expo-status-bar';
+import { RootStackParamList } from './src/navigation';
+import { SplashScreen } from './src/screens/SplashScreen';
+import { LoginScreen } from './src/screens/LoginScreen';
+import { RegisterScreen } from './src/screens/RegisterScreen';
+import { ForgotPasswordScreen } from './src/screens/ForgotPasswordScreen';
+import { AccountCreatedScreen } from './src/screens/AccountCreatedScreen';
+import { PlaceholderScreen } from './src/screens/PlaceholderScreen';
+const Stack = createNativeStackNavigator<RootStackParamList>();
+export default function App() { return <NavigationContainer><StatusBar style="dark"/><Stack.Navigator initialRouteName="Splash" screenOptions={{headerShown:false,animation:'fade'}}><Stack.Screen name="Splash" component={SplashScreen}/><Stack.Screen name="Login" component={LoginScreen}/><Stack.Screen name="Register" component={RegisterScreen}/><Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen}/><Stack.Screen name="AccountCreated" component={AccountCreatedScreen}/><Stack.Screen name="ConnectBox" component={PlaceholderScreen}/><Stack.Screen name="Home" component={PlaceholderScreen}/></Stack.Navigator></NavigationContainer>; }
