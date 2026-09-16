a8042140e4acb657acbca4569857f29f2a7fd2dd
Maia <maianicolecampeanu@gmail.com>
2026-09-16 21:17:26 +0300

    Add ADMO theme colors

---
+++ src/theme/colors.ts
@@ -0,0 +1,1 @@
+export const colors = { navy:'#073B67', teal:'#08A7A2', tealBright:'#14C8C0', textMuted:'#7E96B2', border:'#CFE3EB', background:'#F9FEFF', wave:'#D5F2F3', white:'#FFFFFF', danger:'#D85A5A' };
