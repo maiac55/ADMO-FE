4b6bba0f0d499c219295197691bde0f46dc7d5c0
Maia <maianicolecampeanu@gmail.com>
2026-09-16 21:17:21 +0300

    Add navigation types

---
+++ src/navigation.ts
@@ -0,0 +1,1 @@
+export type RootStackParamList = { Splash: undefined; Login: undefined; Register: undefined; ForgotPassword: undefined; AccountCreated: undefined; ConnectBox: undefined; Home: undefined; };
