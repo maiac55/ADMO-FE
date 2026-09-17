78e8636469f468df4f7b3fef51ca90f7489d0fa3
Maia <maianicolecampeanu@gmail.com>
2026-09-17 14:31:12 +0300

    Add ADMO box setup routes

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,1 +1,11 @@
-export type RootStackParamList = { Splash: undefined; Login: undefined; Register: undefined; ForgotPassword: undefined; AccountCreated: undefined; ConnectBox: undefined; Home: undefined; };
+export type RootStackParamList = {
+  Splash: undefined;
+  Login: undefined;
+  Register: undefined;
+  ForgotPassword: undefined;
+  AccountCreated: undefined;
+  ConnectBox: undefined;
+  PersonInfo: undefined;
+  BoxAdded: { name: string; age: string; note: string };
+  Home: undefined;
+};
