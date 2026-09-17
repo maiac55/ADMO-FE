4e94a0a09ef18c02b16df8e914244db2e866f441
Maia <maianicolecampeanu@gmail.com>
2026-09-17 14:31:19 +0300

    Connect ADMO box setup flow

--- App.tsx
+++ App.tsx
@@ -8,6 +8,28 @@ import { LoginScreen } from './src/screens/LoginScreen';
 import { RegisterScreen } from './src/screens/RegisterScreen';
 import { ForgotPasswordScreen } from './src/screens/ForgotPasswordScreen';
 import { AccountCreatedScreen } from './src/screens/AccountCreatedScreen';
+import { ConnectBoxScreen } from './src/screens/ConnectBoxScreen';
+import { PersonInfoScreen } from './src/screens/PersonInfoScreen';
+import { BoxAddedScreen } from './src/screens/BoxAddedScreen';
 import { PlaceholderScreen } from './src/screens/PlaceholderScreen';
+
 const Stack = createNativeStackNavigator<RootStackParamList>();
-export default function App() { return <NavigationContainer><StatusBar style="dark"/><Stack.Navigator initialRouteName="Splash" screenOptions={{headerShown:false,animation:'fade'}}><Stack.Screen name="Splash" component={SplashScreen}/><Stack.Screen name="Login" component={LoginScreen}/><Stack.Screen name="Register" component={RegisterScreen}/><Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen}/><Stack.Screen name="AccountCreated" component={AccountCreatedScreen}/><Stack.Screen name="ConnectBox" component={PlaceholderScreen}/><Stack.Screen name="Home" component={PlaceholderScreen}/></Stack.Navigator></NavigationContainer>; }
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <StatusBar style="dark" />
+      <Stack.Navigator initialRouteName="Splash" screenOptions={{ headerShown: false, animation: 'fade' }}>
+        <Stack.Screen name="Splash" component={SplashScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
+        <Stack.Screen name="AccountCreated" component={AccountCreatedScreen} />
+        <Stack.Screen name="ConnectBox" component={ConnectBoxScreen} />
+        <Stack.Screen name="PersonInfo" component={PersonInfoScreen} />
+        <Stack.Screen name="BoxAdded" component={BoxAddedScreen} />
+        <Stack.Screen name="Home" component={PlaceholderScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
